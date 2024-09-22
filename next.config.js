d5167d94af734834b3d2ae8198aa9c9df92b0efd
Daniel Rivera <daniel@builtbyhq.com>
2024-09-22 08:26:09 -0600

    Handle Cloudfront headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Forwarded-Host",
+            value: "bombasguadalajara.com",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
